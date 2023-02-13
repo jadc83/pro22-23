5453f338c086e82780a92a2bdeeac7d989ccf4f5
jadc83 <joseantonio.dominguez@iesdonana.org>
2023-02-13 12:49:07 +0100

    Deleted

--- PTM/dado.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def dados(tiradas, caras):
-    total = 0
-    for x in range(1,tiradas):
-        num = random.randint(1,caras)
-        total += num
-    return total
--- PTM/main.py
+++
@@ -1,7 +0,0 @@
-
-from dado import dados
-from character import personaje
-import os
-
-Scorpion = personaje("Scorpion", dados(3), dados(3), dados(3), dados(3), dados(3), dados(3), dados(3), dados(3))
-Subzero = personaje("Subzero", dados(3), dados(3), dados(3), dados(3), dados(3), dados(3), dados(3), dados(3))
