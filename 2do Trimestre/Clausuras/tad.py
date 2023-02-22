a8ad2f84b196a89fbbcd59386e2637095a195548
jadc83 <joseantonio.dominguez@iesdonana.org>
2023-02-22 02:48:02 +0100

    Ejercicio clausuras 22/02/23

---
+++ 2do Trimestre/Clausuras/tad.py
@@ -0,0 +1,10 @@
+
+from creacion_usuario import *
+
+def crear_usuario(nombre, password):
+    """Esta funcion usa a su vez la funcion 'createuser' del 
+       modulo 'creacion_usuario.py' para su funcionamiento."""
+    def usuario():
+        tupla = nombre, password
+        return dict((usr, pwd) for usr, pwd in [tupla])
+    return usuario
--- 2do Trimestre/prueba2.py
+++
@@ -1,4 +0,0 @@
-import random
-
-def suma(*argumentos):
-    return f"Hola, soy la funcion suma, los cojones, {random.randint(1,999)} + {random.randint(1,999)} es {random.randint(1,999)}" 
