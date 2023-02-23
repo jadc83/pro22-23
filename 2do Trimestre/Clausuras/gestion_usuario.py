9e319240299ca98ab34af5ceb7b195c615e7e6f4
jadc83 <joseantonio.dominguez@iesdonana.org>
2023-02-23 23:09:10 +0100

    Update clausura

--- 2do Trimestre/Clausuras/gestion_usuario.py
+++ 2do Trimestre/Clausuras/gestion_usuario.py
@@ -1,2 +1,12 @@
 from userconfig import *
 
+def ayuda():
+    print("-----------------------------------------------------------------------")
+    print("OPCIONES DISPONIBLES:")
+    print("-----------------------------------------------------------------------")
+    print("crea_usuario(nombre, password)                     CREAR USUARIO.")
+    print("listar()                                           LISTAR USUARIOS.")
+    print("cambiar_pass(usuario, password, nuevo password)    CAMBIAR PASSWORD.")
+    print("-----------------------------------------------------------------------")
+    return ayuda
+ayuda()
